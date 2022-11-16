d84614455952291b123869c84274d8aa69baf947
Adeoye <adeolaabeni88@gmail.com>
2022-11-16 07:44:22 -0800

    headers

---
+++ headers/string.h
@@ -0,0 +1,10 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+int _strlen(const char *s);
+int _strcmp(const char *s1, const char *s2);
+char *_strcat(char *dest, const char *src);
+char *_strdup(const char *str);
+int count_str(char **stri);
+
+#endif
