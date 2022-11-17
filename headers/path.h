a794d09878e1c248a40047308f29a9a7c55ed0c9
Giwa <giwatitilope899@gmail.com>
2022-11-17 07:58:11 -0800

    mem

--- headers/path.h
+++
@@ -1,9 +0,0 @@
-#ifndef PATH_H
-#define PATH_H
-
-bool ispath(const char *s);
-char *joinpath(const char *base, const char *child);
-char *basename(const char *path);
-char *dirname(char *path);
-
-#endif
